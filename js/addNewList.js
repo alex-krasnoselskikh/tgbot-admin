7161f4654487e5f1abcc2baed0ac81d001143259
alex-krasnoselskikh <alex-krasnoselskikh@users.noreply.github.com>
2018-04-06 00:02:16 +0300

    implement generation of tables and lists from json

--- js/addNewList.js
+++ js/addNewList.js
@@ -4,7 +4,8 @@ function removeList(e) {
   // console.log('list removed');
 }
 
-function addNewList() {
+function addNewList(list = {}) {
+  const { listname, date, status, isSendedColumn, messagetext, tguser } = list;
   const templateList = `
   <div class="list border border-secondary w-50">
     <div class="col-sm-10">
@@ -13,37 +14,37 @@ function addNewList() {
     <div class="form-group row ml-4">
       <label for="listname" class="col-sm-2 col-form-label">List Name</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="List Name" id="listname" value="А">
+        <input type="text" class="form-control" name="List Name" id="listname" value="${listname || ''}">
       </div>
     </div>
     <div class="form-group row ml-4">
       <label for="date" class="col-sm-2 col-form-label">Date</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="Date" id="date" value="B">
+        <input type="text" class="form-control" name="Date" id="date" value="${date || ''}">
       </div>
     </div>
     <div class="form-group row ml-4">
       <label for="status" class="col-sm-2 col-form-label">Status</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="Status" id="status" value="AN">
+        <input type="text" class="form-control" name="Status" id="status" value="${status || ''}">
       </div>
     </div>
     <div class="form-group row ml-4">
       <label for="is-sended-column" class="col-sm-2 col-form-label">is Sended Column</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="is Sended Column" id="is-sended-column" value="AO">
+        <input type="text" class="form-control" name="is Sended Column" id="is-sended-column" value="${isSendedColumn || ''}">
       </div>
     </div>
     <div class="form-group row ml-4">
       <label for="message-text" class="col-sm-2 col-form-label">Message Text</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="Message Text" id="message-text" value="AP">
+        <input type="text" class="form-control" name="Message Text" id="message-text" value="${messagetext || ''}">
       </div>
     </div>
     <div class="form-group row ml-4">
       <label for="tg-user" class="col-sm-2 col-form-label">Tg User</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="Tg User" id="tg-user" value="AP">
+        <input type="text" class="form-control" name="Tg User" id="tguser" value="${tguser || ''}">
       </div>
     </div>
     <hr />
