c0ad984dac8308306f3cd58ed3adf35eb700571d
alex-krasnoselskikh <alex-krasnoselskikh@users.noreply.github.com>
2018-04-07 05:22:42 +0300

    implement function that collects data from spreadsheets and lists inputs values

--- js/addNewTable.js
+++ js/addNewTable.js
@@ -12,7 +12,7 @@ function removeTable(e) {
 function addNewTable(table = {}) {
   // console.log(table.lists);
   const templateTable = `
-  <div class="spreadsheet border border-primary" id="spredsheet-${table.id || 'iam new'}">
+  <div class="spreadsheet border border-primary" id="spreadsheet-${table.id || 'iam new'}">
     <h3>New Table</h3>
     <div class="col-sm-10">
       <button type="button" class="btn btn-danger" onclick="removeTable(event)">Remove this Table</button>
@@ -20,7 +20,7 @@ function addNewTable(table = {}) {
     <div class="form-group row ml-4">
       <label for="speadsheet-id" class="col-sm-2 col-form-label">ID</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control spreadsheet-input" name="ID" id="speadsheet-id" value="${table.id || ''}">
+        <input type="text" class="form-control spreadsheet-input" name="id" id="speadsheet-id" value="${table.id || ''}">
       </div>
     </div>
     <div class="lists">
