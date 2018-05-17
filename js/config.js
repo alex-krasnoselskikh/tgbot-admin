d23ba0d365617224bcbdd126371751502cc78eff
alex-krasnoselskikh <alex-krasnoselskikh@users.noreply.github.com>
2018-05-17 21:33:32 +0300

    change API URLs to other location

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const
-  configUrl = 'https://tg.cocklife.ru/admin/api/Config/',
-  usersUrl = 'https://tg.cocklife.ru/admin/api/user/',
-  logsUrl = 'https://tg.cocklife.ru/admin/api/log/';
-  tokenUrl = 'https://tg.cocklife.ru/admin/token'
+  configUrl = 'https://legenda.justcrm.pro/admin/api/Config/',
+  usersUrl = 'https://legenda.justcrm.pro/admin/api/user/',
+  logsUrl = 'https://legenda.justcrm.pro/admin/api/log/',
+  tokenUrl = 'https://legenda.justcrm.pro/admin/token';
 const tokenKey = "accessToken";
